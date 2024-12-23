80a2c949ea3f02b8d285c4658c3ff8825b03955e
NewGyu <NewGyu@kinoboku.net>
2024-12-23 15:15:38 +0000

    implement month calendar component and update year page layout

--- app/[year]/page.tsx
+++ app/[year]/page.tsx
@@ -1,13 +1,27 @@
+import MonthCalendar from "@/components/MonthCalendar";
+
 export default async function Page({
-     params 
+     params
 }: {
     params: Promise<{ year: number}>
 }) {
     const { year } = await params;
+    const months = Array.from({ length: 12 }, (_, i) => i + 1);
     return (
         <div>
-            <h1>Year</h1>
-            <h1>{ year }</h1>
+            <div>
+                <h1 className="font-extrabold text-center text-3xl">{ year }年</h1>
+            </div>
+
+            <div>
+            {
+                months.map((month) => (
+                    <div key={month}>
+                        <MonthCalendar year={year} month={month} />
+                    </div>
+                ))
+            }
+            </div>
         </div>
     )
 }
---
+++ components/MonthCalendar.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import styles from './MonthCalendar.module.css';
+
+interface MonthCalendarProps {
+  year: number;
+  month: number;
+}
+
+const MonthCalendar: React.FC<MonthCalendarProps> = ({ year, month }) => {
+  const daysInMonth = new Date(year, month, 0).getDate();
+  // 月の最初の曜日を取得
+  const firstDay = new Date(year, month - 1, 1).getDay();
+  // 先頭をゼロ埋めした日付配列
+  const days = [
+    ...Array.from({ length: firstDay }, () => 0),
+    ...Array.from({ length: daysInMonth }, (_, i) => i + 1)
+  ];
+  // 週ごとに分割された二次元配列
+  const weeks = days.reduce((acc, day) => {
+    if (acc.length === 0 || acc[acc.length - 1].length === 7) {
+      acc.push([]);
+    }
+    acc[acc.length - 1].push(day);
+    return acc;
+  }, [] as number[][]);
+
+  // 週の行コンポーネント
+  const WeekRow: React.FC<{ days: number[] }> = ({ days }) => {
+    return (
+      <tr>
+        {days.map((day, idx) => (
+          <DayCell day={day} key={idx} />
+        ))}
+      </tr>
+    );
+  }
+
+  // 日付セルコンポーネント
+  const DayCell: React.FC<{ day: number }> = ({ day }) => {
+    if (day < 1) {
+      return <td></td>;
+    }
+
+    //曜日を取得
+    const weekday = new Date(year, month - 1, day).getDay();
+    const dateClass = weekday === Weekday.Sunday ? styles.wdSunday : weekday === Weekday.Saturday ? styles.wdSaturday : '';
+    return <td className={`${dateClass} border border-black`}>{day}</td>;
+  }
+
+
+  // 月の最後の曜日までの空白を追加
+  return (
+    <div>
+      <h2 className='font-extrabold text-2xl'>{month}月</h2>
+      <table className='table-fixed border-black border-2'>
+        <thead>
+          <tr>
+            <th className={`${styles.wdSunday} border border-black`}>日</th>
+            <th className='border border-black'>月</th>
+            <th className='border border-black'>火</th>
+            <th className='border border-black'>水</th>
+            <th className='border border-black'>木</th>
+            <th className='border border-black'>金</th>
+            <th className={`${styles.wdSaturday} border border-black`}>土</th>
+          </tr>
+        </thead>
+        <tbody>
+            {weeks.map((week, idx) => (
+              <WeekRow key={idx} days={week} />
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+enum Weekday {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday
+}
+
+export default MonthCalendar;
+
